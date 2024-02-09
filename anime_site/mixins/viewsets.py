4c0bf7dc30f8a6d5a1401980bb58e64f57cd8cd5
Forgot-0 <mr.aisamutdin@mail.ru>
2024-02-09 11:13:13 +0300

    gunicorn

--- anime_site/mixins/viewsets.py
+++ anime_site/mixins/viewsets.py
@@ -14,35 +14,35 @@ class CustomModelViewSet(RetrieveModelMixin, ModelViewSet):
 
     def list(self, request, *args, **kwargs):
         cache_key = f'{self.basename}_{self.action}'
-        
+
         data = cache.get(cache_key)
         if data:
             return Response(data)
-        
+
         response = super().list(request, *args, **kwargs)
 
         cache.set(cache_key, response.data, timeout=2) 
         return response
-    
+
     def retrieve(self, request, *args, **kwargs):
         cache_key = f'{self.basename}_{self.kwargs["pk"]}'
         data = cache.get(cache_key)
         if data:
             return Response(data)
-        
+
         response = super().retrieve(request, *args, **kwargs)
 
         cache.set(cache_key, response.data, timeout=60) 
         return response
-    
+
     def create(self, request, *args, **kwargs):
         cache.delete(f'{self.basename}_list')
         return super().create(request, *args, **kwargs)
-    
+
     def update(self, request, *args, **kwargs):
         cache.delete(f'{self.basename}_{self.kwargs["pk"]}')
         return super().update(request, *args, **kwargs)
-    
+
     def partial_update(self, request, *args, **kwargs):
         cache.delete(f'{self.basename}_{self.kwargs["pk"]}')
         return super().partial_update(request, *args, **kwargs)
